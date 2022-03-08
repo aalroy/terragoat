981d64fc54932ffb8c41fa8a23d671131fd0a623
aalroy <98309142+aalroy@users.noreply.github.com>
2022-03-08 10:27:04 +0200

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -36,6 +36,7 @@ resource "aws_ebs_volume" "web_host_storage" {
   availability_zone = "${var.region}a"
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size = 1
+  
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs"
     }, {
@@ -108,6 +109,7 @@ resource "aws_security_group" "web-node" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
+    
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "b7af1b40-64eb-4519-a1a0-ab198db4b193"
