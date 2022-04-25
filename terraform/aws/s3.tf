7d5c7006c8855beb377eb6739609c4a8283a121c
aalroy <98309142+aalroy@users.noreply.github.com>
2022-04-25 11:48:52 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -124,14 +124,7 @@ resource "aws_s3_bucket" "logs" {
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
-    }
-  }
+
   force_destroy = true
   tags = merge({
     Name        = "${local.resource_prefix.value}-logs"
